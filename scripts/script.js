dde32555bf76ae05b77ca1dc59a1b6b6f65bca87
CreepyTeabag <124189089+CreepyTeabag@users.noreply.github.com>
2023-03-18 11:58:45 +0300

    Исправлен JS гамбургер-меню, добавлено меню

--- scripts/script.js
+++ scripts/script.js
@@ -3,18 +3,14 @@
 let hamMenu = document.querySelector('.header-hamburger-menu');
 let hamSquares = document.querySelector('.header-hamburger-squares');
 let hamArrows = document.querySelector('.header-hamburger-arrows');
+let menu = document.querySelector('.menu');
 
 
-addEventListener("click", (event) => {
-   if( hamSquares.classList.contains('ham-show') ) {
-      hamSquares.classList.remove('ham-show');
-      hamSquares.classList.add('ham-hide');
-      hamArrows.classList.remove('ham-hide');
-      hamArrows.classList.add('ham-show');
-   } else {
-      hamArrows.classList.remove('ham-show');
-      hamArrows.classList.add('ham-hide');
-      hamSquares.classList.remove('ham-hide');
-      hamSquares.classList.add('ham-show');
-   }
+hamMenu.addEventListener("click", (event) => {
+   hamSquares.classList.toggle('ham-show');
+   hamSquares.classList.toggle('ham-hide');
+   hamArrows.classList.toggle('ham-hide');
+   hamArrows.classList.toggle('ham-show');
+   menu.classList.toggle('ham-hide');
+   menu.classList.toggle('ham-show');
 });
