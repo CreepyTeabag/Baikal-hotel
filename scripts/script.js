a457b7614fe8067859eb287e10be165c2ea2818c
CreepyTeabag <124189089+CreepyTeabag@users.noreply.github.com>
2023-03-18 12:37:59 +0300

    Сделана навигация

--- scripts/script.js
+++ scripts/script.js
@@ -4,13 +4,19 @@ let hamMenu = document.querySelector('.header-hamburger-menu');
 let hamSquares = document.querySelector('.header-hamburger-squares');
 let hamArrows = document.querySelector('.header-hamburger-arrows');
 let menu = document.querySelector('.menu');
+let allMenuLinks = document.querySelectorAll('.menu-link');
 
-
-hamMenu.addEventListener("click", (event) => {
+let menuToggle = function() {
    hamSquares.classList.toggle('ham-show');
    hamSquares.classList.toggle('ham-hide');
    hamArrows.classList.toggle('ham-hide');
    hamArrows.classList.toggle('ham-show');
    menu.classList.toggle('ham-hide');
    menu.classList.toggle('ham-show');
+};
+
+hamMenu.addEventListener('click', menuToggle);
+
+allMenuLinks.forEach(function (menuLink) {
+   menuLink.addEventListener('click', menuToggle);
 });
