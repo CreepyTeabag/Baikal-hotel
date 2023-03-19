7b49d5ece274bba26f01f7e5ba43f357c7169527
CreepyTeabag <124189089+CreepyTeabag@users.noreply.github.com>
2023-03-19 20:30:44 +0300

    Подключён eslint, исправлены ошибки

--- scripts/script.js
+++ scripts/script.js
@@ -1,19 +1,22 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable strict */
+
 'use strict';
 
-let hamMenu = document.querySelector('.header-hamburger-menu');
-let hamSquares = document.querySelector('.header-hamburger-squares');
-let hamArrows = document.querySelector('.header-hamburger-arrows');
-let menu = document.querySelector('.menu');
-let allMenuLinks = document.querySelectorAll('.menu-link');
+const hamMenu = document.querySelector('.header-hamburger-menu');
+const hamSquares = document.querySelector('.header-hamburger-squares');
+const hamArrows = document.querySelector('.header-hamburger-arrows');
+const menu = document.querySelector('.menu');
+const allMenuLinks = document.querySelectorAll('.menu-link');
 
-let menuToggle = function() {
-   hamSquares.classList.toggle('ham-show');
-   hamArrows.classList.toggle('ham-show');
-   menu.classList.toggle('ham-show');
+const menuToggle = () => {
+  hamSquares.classList.toggle('ham-show');
+  hamArrows.classList.toggle('ham-show');
+  menu.classList.toggle('ham-show');
 };
 
 hamMenu.addEventListener('click', menuToggle);
 
-allMenuLinks.forEach(function (menuLink) {
-   menuLink.addEventListener('click', menuToggle);
+allMenuLinks.forEach((menuLink) => {
+  menuLink.addEventListener('click', menuToggle);
 });
