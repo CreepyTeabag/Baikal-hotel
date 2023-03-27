be96c99320aa9ca0098bc7d9518421002f323f13
CreepyTeabag <124189089+CreepyTeabag@users.noreply.github.com>
2023-03-27 20:37:36 +0300

    Подключён JavaScript к popup

--- scripts/script.js
+++ scripts/script.js
@@ -20,3 +20,29 @@ hamMenu.addEventListener('click', menuToggle);
 allMenuLinks.forEach((menuLink) => {
   menuLink.addEventListener('click', menuToggle);
 });
+
+const allCloseButtons = document.querySelectorAll('.popup-close');
+const allSendButtons  = document.querySelectorAll('.button-send');
+const allCallbackButtons = document.querySelectorAll('.button-callback');
+const allBookButtons = document.querySelectorAll('.button-book');
+const popupWindow = document.querySelector('.popup');
+
+const popupToggle = () => {
+  popupWindow.classList.toggle('popup-show');
+};
+
+allCloseButtons.forEach((closeButton) => {
+  closeButton.addEventListener('click', popupToggle);
+});
+
+allCallbackButtons.forEach((callbackButton) => {
+  callbackButton.addEventListener('click', popupToggle);
+});
+
+allBookButtons.forEach((bookButton) => {
+  bookButton.addEventListener('click', popupToggle);
+});
+
+allSendButtons.forEach((sendButton) => {
+  sendButton.addEventListener('click', popupToggle);
+});
