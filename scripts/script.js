abd39245c86cccb4aa84dc738414805da9199c43
CreepyTeabag <124189089+CreepyTeabag@users.noreply.github.com>
2023-04-02 17:18:51 +0300

    Свёрстана и адаптирована секция popup-success + JS

--- scripts/script.js
+++ scripts/script.js
@@ -21,28 +21,42 @@ allMenuLinks.forEach((menuLink) => {
   menuLink.addEventListener('click', menuToggle);
 });
 
-const allCloseButtons = document.querySelectorAll('.popup-close');
-const allSendButtons  = document.querySelectorAll('.button-send');
+menu.querySelector('.button-callback').addEventListener('click', menuToggle);
+
+const closePopupForm = document.querySelectorAll('.popup-form-close');
+const closeSuccessForm = document.querySelectorAll('.popup-success-close');
+const allSendButtons = document.querySelectorAll('.button-send');
 const allCallbackButtons = document.querySelectorAll('.button-callback');
 const allBookButtons = document.querySelectorAll('.button-book');
-const popupWindow = document.querySelector('.popup');
+const popupForm = document.querySelector('.popup-form-block');
+const popupSuccess = document.querySelector('.popup-success');
 
-const popupToggle = () => {
-  popupWindow.classList.toggle('popup-show');
+const popupFormToggle = () => {
+  popupForm.classList.toggle('popup-show');
+};
+const popupSuccessToggle = () => {
+  popupSuccess.classList.toggle('popup-show');
 };
 
-allCloseButtons.forEach((closeButton) => {
-  closeButton.addEventListener('click', popupToggle);
-});
-
 allCallbackButtons.forEach((callbackButton) => {
-  callbackButton.addEventListener('click', popupToggle);
+  callbackButton.addEventListener('click', popupFormToggle);
 });
 
 allBookButtons.forEach((bookButton) => {
-  bookButton.addEventListener('click', popupToggle);
+  bookButton.addEventListener('click', popupFormToggle);
+});
+
+closePopupForm.forEach((closeButton) => {
+  closeButton.addEventListener('click', popupFormToggle);
+});
+
+closeSuccessForm.forEach((closeButton) => {
+  closeButton.addEventListener('click', popupSuccessToggle);
 });
 
 allSendButtons.forEach((sendButton) => {
-  sendButton.addEventListener('click', popupToggle);
+  sendButton.addEventListener('click', popupFormToggle);
+  sendButton.addEventListener('click', popupSuccessToggle);
 });
+
+// popupWindow.querySelector('input').value('');
